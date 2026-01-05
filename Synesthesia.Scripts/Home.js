3b4342afc13d4d9f0caf47bb2cbede90da00d835
DarttGoblin <yassine.bazgour@gmail.com>
2026-01-05 22:03:38 +0100

    Added read report button

--- Synesthesia.Scripts/Home.js
+++ Synesthesia.Scripts/Home.js
@@ -1,5 +1,6 @@
 const upload = document.querySelector('.upload');
 const github = document.querySelector('.github');
+const report = document.querySelector('.report');
 const demo = document.querySelector('.demo');
 
 upload.onclick = function() {
@@ -8,6 +9,10 @@ upload.onclick = function() {
 
 github.onclick = function() {
     window.open('https://github.com/DarttGoblin/Synesthesia_server', '_blank');
+}
+
+report.onclick = function() {
+    window.open('https://drive.google.com/file/d/1VYHbbJppJZpfYNXbxCFizb_qtsHfdLvh/view?usp=sharing', '_blank');
 }
 
 demo.onclick = function() {
